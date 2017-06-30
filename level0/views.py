0a7a520bcd70422810d627e0837466cd8980010c
Matt Evans <Matt.Evans@iag.co.nz>
2017-06-30 14:53:43 +1200

    Basic project selection done New project functionality added Delete + View project done

--- level0/views.py
+++ level0/views.py
@@ -20,6 +20,16 @@ def new_project(request):
     }
     return HttpResponse(template.render(context, request))
 
+def list_project(request):
+    template = loader.get_template('level0/threats.html')
+    projectList = get_projects()
+    context = {
+        'container': "level0/project-list.html",
+        'id': 0,
+        'projectList': projectList
+    }
+    return HttpResponse(template.render(context, request))
+
 def threats(request, id=0):
     template = loader.get_template('level0/threats.html')
     context = {
@@ -31,7 +41,11 @@ def threats(request, id=0):
 def get_json_model(request, id=0):
     driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "neon40j"))
     session = driver.session()
-    result = session.run("MATCH (p:project)-[r:RiskOf]-(t:Threat)-[m:Mitigation]-(c:Control) WHERE p.id = $id RETURN p.name as projectName, t.threat as threat, collect(c) as control", id=int(id))
+    query = "MATCH(p: Project)-[r: RiskOf]-(t: Threat) WHERE  p.id = $id"
+    query +=" OPTIONAL MATCH(t)-[m: Mitigation]-(c: Control)"
+    query +=" RETURN p.name as projectName, t.threat as threat, collect(c) as control"
+
+    result = session.run(query, id=int(id))
     session.close()
     db = connect_db() #sqlite
 
@@ -81,15 +95,61 @@ def get_json_threats(request):
 
     return HttpResponse(json.dumps(nodes), content_type="application/json")
 
-def contains (list, filter):
-    for x in list:
-        if filter(x):
-            return True;
-        return False;
-
 def connect_db():
     """Connects to the specific database."""
     databaselocation = DATABASES['default']['NAME']
     rv = sqlite3.connect(databaselocation)
     rv.row_factory = sqlite3.Row
     return rv
+
+def add_project(request):
+    if request.method == 'GET':
+        #TODO: transactional + exception handling
+        projectName = request.GET['projectName']
+
+        db = connect_db()  # sqlite
+        cur = db.cursor()
+        result = cur.execute('INSERT INTO projects (Name) VALUES(?)', [projectName])
+        projectId = result.lastrowid
+        db.commit()
+
+        query = "MATCH (t:Threat) WHERE t.threatId IN [{0}]".format(request.GET['threatList'])
+        query += " CREATE (p:Project{{name: '{0}', id:$id}})".format(projectName)
+        query += " CREATE (p)-[:RiskOf]->(t)"
+
+        driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "neon40j"))
+        session = driver.session()
+        session.run(query, id=int(projectId))
+
+    return HttpResponse('{{"success":true, "projectId": {0}}}'.format(projectId))
+
+def get_projects():
+    db = connect_db()
+    cur = db.execute('SELECT * FROM projects')
+    projects = []
+    for record in cur.fetchall():
+        projects.append({"id": record['id'], "projectName": record['name']})
+
+    return projects
+
+def delete_project(request, id=0):
+    #TODO: For the moment project id cannot be removed as it is a Demo project
+    #plus there is no security or restrictions so uploading data would be a pain
+    projectId = int(id)
+    if projectId != 1:
+        #TODO: Transaction again + exception handling
+        db = connect_db()
+        db.execute('DELETE FROM projects WHERE id =?',[projectId])
+        db.commit()
+
+        query = "MATCH (p:Project) WHERE p.id = $id DETACH DELETE p"
+
+        driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "neon40j"))
+        session = driver.session()
+        session.run(query, id=int(projectId))
+
+    return HttpResponse('{{"deleted":true, "projectId": {0}}}'.format(projectId))
+
+
+
+
